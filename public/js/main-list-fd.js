cba1d80398dddacaceb39b66647406a18810ba18
BADEnder <s10183001@gmail.com>
2023-03-30 00:35:01 +0800

    excel by chatgpt

--- public/js/main-list-fd.js
+++ public/js/main-list-fd.js
@@ -31,18 +31,12 @@ const delRows = (elemId) => {
 const submitRows = () => {
     const allPno = document.querySelectorAll("#pno")
     const allTotalPieces = document.querySelectorAll("#total-pieces")
-    // console.log(typeof allPno)
-    // console.log(Object.keys(allPno))
+
     
     for (let idx=0; idx< allPno.length; idx++) {
         let itemPno = allPno[idx]
         let itemTP = allTotalPieces[idx]
-        // console.log(idx)
-        // console.log(itemPno)
-        // console.log(itemTP)
-        // console.log('-----')
-        // console.log(itemPno.value)
-        // console.log(itemTP.value)
+
         let object = {
             "pno": itemPno.value,
             "total_pieces": Number(itemTP.value)
@@ -52,36 +46,57 @@ const submitRows = () => {
 
 
     postData('http://127.0.0.1/api/postData', data)
+    data = []
+    // postData2('http://127.0.0.1/api/postData', data)
+}
+const submitExcel = () => {
+    const excel_elem = document.querySelector("#excel")
+
+
+    
     // postData2('http://127.0.0.1/api/postData', data)
 }
 
+function handleFile() {
+    // 獲取選擇的檔案
+    var file = document.getElementById("input").files[0];
+
+    // 讀取Excel檔案
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      var data = event.target.result;
+      var workbook = XLSX.read(data, {type: 'binary'});
+      var sheetName = workbook.SheetNames[0];
+      var sheet = workbook.Sheets[sheetName];
+
+      // 將Excel資料轉換為HTML表格
+      var html = XLSX.utils.sheet_to_html(sheet);
+      document.getElementById('output').innerHTML = html;
+    };
+    reader.readAsBinaryString(file);
+}
 
 const postData = async(url, data) => {
     // console.log(typeof data)
     // console.log(data)
     const res = await fetch(url, {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
+        // mode: "cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit
         headers: {
           "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // redirect: "follow", // manual, *follow, error
+        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data),
     })
 
-    console.log(res.data)
-    console.log(res.body)
-    console.log(res)
+    
 
 }
 
 // const postData2 = async(url, data) => {
-//     console.log(typeof data)
-//     console.log(data)
 //     const res = await axios(url, {
 //         method: "POST",
 //         // headers: {
@@ -92,9 +107,5 @@ const postData = async(url, data) => {
 //         data: data
 //     })
 
-//     console.log(res.data)
-//     // console.log(res.json)
-//     // console.log(res.body)
-//     // console.log(res.body.msg)
-//     // console.log(res.json())
+
 // }
